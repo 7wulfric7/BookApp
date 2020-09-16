97124904f2d9d284ff7da531f3add9bc5aacab69
Deniz Adil <xdenizadil@gmail.com>
2020-09-16 10:14:03 +0200

    Added two new text Labels Added two new Text Fields

--- BookApp/BookApp/ViewController.swift
+++ BookApp/BookApp/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var txtFisrtName: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtCountry: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         txtFisrtName.delegate = self
+        txtAddress.delegate = self
+        txtCountry.delegate = self
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
